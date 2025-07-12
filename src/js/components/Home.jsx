b49eaf3e32ab95131d11c2ad3b2b886ecd807ed6
Angel-enc <enock8876@gmail.com>
2025-07-12 17:48:05 +0000

    terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [seleccionColor, setSeleccionColor] = useState("red");
+	const [mostrarPurpura, setMostrarPurpura] = useState(false);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const cambioColor = () => {
+		const colores = ["red", "yellow", "green", mostrarPurpura && "purple"].filter(Boolean);
+		const indexActual = colores.indexOf(seleccionColor);
+		const siguienteIndex = (indexActual + 1) % colores.length;
+		setSeleccionColor(colores[siguienteIndex]);
+	};
+
+	const añadirPurpura = () => {
+		setMostrarPurpura(true);
+	};
+
+	return (
+		<>
+			<div className="baseSemaforo pt-5">
+				<div className="semaforo">
+					<div
+						onClick={() => setSeleccionColor("red")}
+						className={`light red ${seleccionColor === "red" ? "glow" : ""}`}
+					></div>
+					<div
+						onClick={() => setSeleccionColor("yellow")}
+						className={`light yellow ${seleccionColor === "yellow" ? "glow" : ""}`}
+					></div>
+					<div
+						onClick={() => setSeleccionColor("green")}
+						className={`light green ${seleccionColor === "green" ? "glow" : ""}`}
+					></div>
+					{mostrarPurpura && (
+						<div
+							onClick={() => setSeleccionColor("purple")}
+							className={`light purple ${seleccionColor === "purple" ? "glow" : ""}`}
+						></div>
+					)}
+				</div>
+			</div>
+				<div className="mastil"></div>
+			<div className="Botones">
+				<button className="btn btn-success" onClick={cambioColor}>Cambio de color</button>
+				<button className="btn btn-success" onClick={añadirPurpura}>Añadir nueva luz</button>
+			</div>
+		</>
 	);
 };
 
